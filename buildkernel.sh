0ac81ce9401e58264bb8b4df987d8200dffe4084
Stilzchen <49190935+Stilzchen@users.noreply.github.com>
2019-04-02 17:07:40 +0200

    Update buildkernel.sh

--- buildkernel.sh
+++ buildkernel.sh
@@ -13,7 +13,8 @@ echo -e "##################################################
 #  please be shure to mount /boot                #
 #  before starting this script                   #
 #                                                #
-#  current longterm kernel version: \t$longtermkernel  #\n#  running kernel version: \t\t$runningkernel  #
+#  current longterm kernel version: \t$longtermkernel  #
+#  running kernel version: \t\t$runningkernel  #
 #                                                #
 ##################################################
 "
@@ -49,12 +50,12 @@ echo "Build everything"
 make -j3 zImage; make -j3 dtbs; make -j3 modules; make modules_install INSTALL_MOD_PATH=
 
 echo "copy zImage and dtb file to one file"
-cat arm/boot/zImage arch/arm/boot/dts/dove-cubox.dtb > zImage.cubox
+cat arch/arm/boot/zImage arch/arm/boot/dts/dove-cubox.dtb > zImage.cubox
 popd
 
 echo "copy file to /boot directory"
 mkimage -A arm -O linux -C none  -T kernel -a 0x00008000 -e 0x00008000 -n 'Linux-cubox' -d /usr/src/linux-$longtermkernel/zImage.cubox /boot/dove-cubox-$longtermkernel-uImage
-echo -e "\nYEAH, i will do it!\n"
+echo -e "\nYEAH, it's done!\n\nPlease be shure to update your boot.src file or change kernel symlinks to your new kernel."
 }
 
 
