07c3003849076aaf2d6c30a87f48a2a56da3bd1e
Stilzchen <49190935+Stilzchen@users.noreply.github.com>
2025-01-03 18:06:29 +0000

    Update buildkernel.sh
    multiple updates
    
    * it is possible to choose a kernel
    * works for older and newer kernels with new file pathes

--- buildkernel.sh
+++ buildkernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-longtermkernel=$(curl -s https://www.kernel.org|grep -A2 longterm|head -n 2|tail -n1 |cut -d '<' -f3|cut -d '>' -f2)
-longtermmajor=$(echo $longtermkernel|cut -d '.' -f1)
+newestlongtermkernel=$(curl -s https://www.kernel.org|grep -A2 longterm|head -n 2|tail -n1 |cut -d '<' -f3|cut -d '>' -f2)
+longtermmajor=$(echo $newestlongtermkernel|cut -d '.' -f1)
 runningkernel=$(uname -r)
 
 
@@ -12,73 +12,122 @@ echo -e "#########################################################
 #  please be shure to mount /boot			#
 #  before starting this script				#
 #							#
-#  current longterm kernel version:\t$longtermkernel		#
+#  current longterm kernel version:\t$newestlongtermkernel		#
 #  running kernel version:\t\t$runningkernel		#
 #							#
 #########################################################
 "
 
 function helpFunction {
-echo -e "\n\nThis script tries to install the newest longterm kernel.\n
+echo -e "----------------------------------------------------------------\n
+Help:\n
+This script tries to install the newest longterm kernel.\n
 Its not part of this script to let you choose the longterm kernel branch.\n\n
 The script checks the version of the newest available longterm kernel, downloads the source, let you modify the cnfig file if needed, build zImage, dtb files and modules and copy kernel to /boot and modules to the /lib directory.\n
 The used kernel config file is copied from the running kernel.\nThe original kernel config file was grapped from xilka kernel 4.10.17 (http://xilka.com/kernel/4/4.10/4.10.17/release/1/), because this one is well prepared and come with many useful modules enabled.\n\n
-As an alternative you can rebuild your running kernel and edit your config first.\n"
+As an alternative you can rebuild your running kernel or define a specific longterm kernel version and edit your config first.\n\n
+----------------------------------------------------------------\n"
 }
 
-function startnewkernelFunction {
+function startnewestkernelFunction () {
 #read -p "Press enter to continue"
 #get the kernel
 echo "Get the new source and unpack it"
-echo $longtermkernel
-echo -e "curl -o /usr/src/linux-$longtermkernel.tar.xz https://cdn.kernel.org/pub/linux/kernel/v$longtermmajor.x/linux-$longtermkernel.tar.xz"
-curl -o /usr/src/linux-$longtermkernel.tar.xz https://cdn.kernel.org/pub/linux/kernel/v$longtermmajor.x/linux-$longtermkernel.tar.xz
+echo -e "We use kernel \033[1;92m"$newestlongtermkernel"\033[0m\n"
+echo -e "curl -o /usr/src/linux-$newestlongtermkernel.tar.xz https://cdn.kernel.org/pub/linux/kernel/v$longtermmajor.x/linux-$newestlongtermkernel.tar.xz"
+curl -o /usr/src/linux-$newestlongtermkernel.tar.xz https://cdn.kernel.org/pub/linux/kernel/v$longtermmajor.x/linux-$newestlongtermkernel.tar.xz
 
 #unpack the kernel
 pushd /usr/src/
-tar xfv linux-$longtermkernel.tar.xz
+tar xfv linux-$newestlongtermkernel.tar.xz
 rm *.tar.xz
 popd
 
 #copy running config to the new kernel source
-zcat /proc/config.gz >> /usr/src/linux-$longtermkernel/.config
+zcat /proc/config.gz >> /usr/src/linux-$newestlongtermkernel/.config
 }
 
-function noconfigFunction {
+function definekernelFunction () {
+echo "These are the current longterm kernel releases"
+definedkernel=$(curl -s https://www.kernel.org|grep -A2 longterm|grep strong|cut -d '<' -f3|cut -d '>' -f2)
+echo -e "\033[1;92m"$definedkernel"\033[0m\n"
+echo -e "Type a valid kernel version number. It is recommended to choose a current longterm kernel version from above\n\n"
+read -p "Please type a valid kernel version as your favorite kernel to build: "
+
+REPLYMAJOR=$(echo $REPLY | cut -d '.' -f1)
+REPLYCHECK=$(curl --silent --head  https://cdn.kernel.org/pub/linux/kernel/v$REPLYMAJOR.x/linux-$REPLY.tar.xz | awk '/^HTTP/{print $2}')
+if [ $REPLYCHECK != 200 ]; then
+ echo "No valid kernel, please choose an option:"
+ read -p "d (define another kernel version), s (start script again) " choice
+ echo -e "\n"
+ case "$choice" in 
+   d|D ) echo "define another kernel version" && definekernelFunction;;
+   s|S ) echo "start script again " && helpFunction && caseFunction;;
+   e|E ) echo "Ok, good bye";;
+   * ) echo -e "\ninvalid input, please type d (define), s (start) or e (exit)\nWe will restart script now.\n\n" && caseFunction;;
+ esac;
+ else
+ echo -e "\n\033[1;37m"Ok, we will use kernel version "\033[1;92m"$REPLY"\033[0m\n"
+
+ # declare kernel for next steps
+ kernel=$(echo $REPLY)
+fi
+}
+
+function startdefinedkernelFunction () {
+REPLYMAJOR=$(echo $REPLY | cut -d '.' -f1)
+echo "Get the new source and unpack it"
+echo -e "We use kernel \033[1;92m"$REPLY"\033[0m\n"
+echo -e "curl -o /usr/src/linux-$REPLY.tar.xz https://cdn.kernel.org/pub/linux/kernel/v$REPLYMAJOR.x/linux-$REPLY.tar.xz"
+curl -o /usr/src/linux-$REPLY.tar.xz https://cdn.kernel.org/pub/linux/kernel/v$REPLYMAJOR.x/linux-$REPLY.tar.xz
+
+#unpack the kernel
+pushd /usr/src/
+tar xfv linux-$REPLY.tar.xz
+rm *.tar.xz
+popd
+
+#copy running config to the new kernel source
+zcat /proc/config.gz >> /usr/src/linux-$REPLY/.config
+}
+
+function noconfigFunction () {
 #go to the source main directory
 pushd /usr/src/linux-$kernel
 echo "Merge config by using oldconfig ..."
 yes "" | make oldconfig
 }
 
-function menuconfigFunction {
+function menuconfigFunction () {
 #go to the source main directory
 pushd /usr/src/linux-$kernel
 echo "starting  menuconfig ..."
 make menuconfig
 }
 
-function xconfigFunction {
+function xconfigFunction () {
 #go to the source main directory
 pushd /usr/src/linux-$kernel
 echo "starting xconfig ..."
 make xconfig
 }
 
-function nconfigFunction {
+function nconfigFunction () {
 #go to the source main directory
 pushd /usr/src/linux-$kernel
 echo "starting nconfig ..."
 make nconfig
 }
 
-function buildFunction {
+function buildFunction () {
 pushd /usr/src/linux-$kernel
 echo "Build everything"
 make -j3 zImage; make -j3 dtbs; make -j3 modules; make modules_install INSTALL_MOD_PATH=
 
-echo "copy zImage and dtb file to one file"
-cat arch/arm/boot/zImage arch/arm/boot/dts/dove-cubox.dtb > zImage.cubox
+echo -e "\ncopy zImage and dtb file to one file"
+kernelimage=$(find /usr/src/linux-$kernel/ -iname zImage)
+kerneldtb=$(find /usr/src/linux-$kernel/ -iname dove-cubox.dtb)
+cat $kernelimage $kerneldtb > zImage.cubox
 popd
 
 echo "copy file to /boot directory"
@@ -86,14 +135,14 @@ mkimage -A arm -O linux -C none  -T kernel -a 0x00008000 -e 0x00008000 -n 'Linux
 echo -e "\nYEAH, it's done!\n\nPlease be shure to update your boot.src file or change kernel symlinks to your new kernel."
 }
 
-function caseFunction ()
-{
-if [ $longtermkernel != $runningkernel ]; then
+function caseFunction () {
+if [ $newestlongtermkernel != $runningkernel ]; then
  echo -e "Do you want to build the new longterm kernel?\n"
- read -p "Please type y (yes), n (no), r (rebuild) or h (help). " choice
+ read -p "Please type y (yes), n (no), r (rebuild), d (define another longterm kernel version) or h (help). " choice
  case "$choice" in 
-   y|Y ) echo "yes" && kernel=$(echo $longtermkernel) && startnewkernelFunction && prepareFunction;;
+   y|Y ) echo "yes" && kernel=$(echo $newestlongtermkernel) && startnewestkernelFunction && prepareFunction;;
    r|R ) echo "rebuild" && kernel=$(echo $runningkernel) && prepareFunction;;
+   d|D ) echo "define" && definekernelFunction && startdefinedkernelFunction && prepareFunction;;
    h|H ) echo "help" && helpFunction && caseFunction;;
    n|N ) echo "Ok, good bye";;
    * ) echo -e "\ninvalid input, please type y (yes), n (no), r (rebuild) or h (help)\n" && caseFunction;;
